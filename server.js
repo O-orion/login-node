7b3d275a2b4605ad86d26e8b448337cef349d58d
O-orion <lucas.lvtorres@gmail.com>
2024-01-07 19:29:09 -0300

    configurando express

---
+++ server.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/extensions
+const app = require('./src/app.js');
+
+const PORT = 8800;
+
+app.listen(PORT, () => {
+  console.log('Servidor rodando ');
+});
---
+++ src/app.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const express = require('express');
+
+const app = express();
+
+module.exports = app;
